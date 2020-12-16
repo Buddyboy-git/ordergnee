76bd9831cfc1a20c16ec68e8416ff6f49c2f6149
Mike Trotta <miketrotta@gmail.com>
2020-12-16 17:15:02 -0500

    git shit 2

---
+++ gitshit.bat
@@ -0,0 +1,8 @@
+echo "# ordergnee" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add github https://github.com/Buddyboy-git/ordergnee.git
+git push -u github main
+                
